9a2a7d62e54fc1af090b15349ecee54c7f991575
Yoos99 <yoosh2520@naver.com>
2024-09-01 01:29:31 +0900

    feat: 헤더 추가

---
+++ src/App.js
@@ -0,0 +1,35 @@
+// export default class App {
+//   constructor() {
+//     this.el = document.createElement('div');
+//     this.el.textContent = 'Hello, World!';
+//   }
+// }
+import { Component } from './core/cat.js';
+
+export default class App extends Component {
+  constructor() {
+    super({
+      state: {
+        inputText: '',
+      },
+    });
+  }
+
+  render() {
+    this.el.classList.add('search');
+    this.el.innerHTML = /*html*/ `
+    <input />
+    <button>Click!!</button>`;
+
+    const inputEl = this.el.querySelector('input');
+    inputEl.addEventListener('input', () => {
+      this.state.inputText = inputEl.value;
+    });
+
+    const buttonEl = this.el.querySelector('button');
+
+    buttonEl.addEventListener('click', () => {
+      console.log(this.state.inputText);
+    });
+  }
+}
